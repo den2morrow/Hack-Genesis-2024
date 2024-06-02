4b317f2fc401487be8f81177f388dd4975bf4b27
Den Tomorrow <whattomson@gmail.com>
2024-06-02 07:30:19 +0300

    Обновил до финальной версии вариант rag system

--- scripts/yandex_llm.py
+++ scripts/yandex_llm.py
@@ -53,17 +53,21 @@ def load_embeddings(folder_path: str, index_name: str) -> FAISS:
 
 
 def rag_system(query: str, db_folder_path: str, db_index_name: str):
-    print(" + Индексируем документы")
+    # print(" + Индексируем документы")
     # db = create_embeddings_with_text('../files/dbs/db1_text', index_name='finance_docs') Создаем базу данных, если ее нет
     # db = create_embeddings_with_pdf('../files/dbs/db1_pdf', index_name='finance_docs') Второй варинант создания без custom pdf_parser
     db =  load_embeddings(folder_path=db_folder_path, index_name=db_index_name)
     retriever = db.as_retriever()
 
-    print(f"+ Запрос: {query}")
-    print(" + Получение релевантных документов")
+    # print(f"+ Запрос: {query}")
+    # print(" + Получение релевантных документов")
     retriever = db.as_retriever()
+    # docs = retriever.invoke(query) На будущее
+    # print(docs[0].page_content) по этому print можно будет определить в каком месте плохо сформирован текст pdf парсером
 
-    template = """Ответь на вопрос, основываясь только на текст ниже:
+    template = """Ответь на вопрос, основываясь только на текст ниже. Плюс сначала пиши название текста и через 2 строчки ответ.
+    Еще учитывай, что ты отлично разбираешься в финансовых отчетах и бухгалтерском учете в целом.
+    Также помни ты получаешь данные из нескольких документов в общей базе данных:
     {context}
 
     Вопрос: {question}
@@ -79,7 +83,7 @@ def rag_system(query: str, db_folder_path: str, db_index_name: str):
         | StrOutputParser()
     )
 
-    print(" + Запуск LLM Chain")
+    # print(" + Запуск LLM Chain")
     res = chain.invoke(query)
     print(f" + Ответ: {res}")
 
